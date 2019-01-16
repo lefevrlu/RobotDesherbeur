82e2296fe0b2cb9f713c2a092419e26b0d5caeff
Chojnacki Alexandre <alexandre.04@hotmail.fr>
2019-01-16 10:15:33 +0100

    ajout du script python renvoyant l'angle et le rayon de l'herbe par rapport au robot | nettoayge du script 'detecte_herbes.py'

--- detect_herbes/detecte_herbes.py
+++ detect_herbes/detecte_herbes.py
@@ -1,39 +1,54 @@
+# -*- coding: utf-8 -*-
 import cv2
 
+def get_bounding_box(img,disp = False):
+	"""
+	input:
+		img: adresse de l'image
+		disp: booléen, enregistre une image de l'object détecté si vrai
+	output:
+		* les coordonées sont dans le repère image usuel *
+		x: coordonnée x du coin supérieur gauche de la bounding box (en pixels depuis la bordure gauche de l'image)
+		y: coordonnée y du coin supérieur gauche de la bounding box (en pixels depuis la bordure supérieure de l'image)
+		w: largeur de la bounding box (en pixels)
+		h: hauteur de la bounding box (en pixels)
+	"""
+	Img = cv2.imread(img)
+	# get dimensions
+	imageHeight, imageWidth, imageChannels = Img.shape
 
-l = 0.1
+	# lire image en HSV
+	hsv = cv2.cvtColor(Img, cv2.COLOR_BGR2HSV)
 
-# read image from file
-Img = cv2.imread("cylindre3.jpg")
+	# interval HSV of green
+	#(120,100,50)
+	greenLow = (40, 40,40) #(36,0,0) 
+	greenUp = (70, 255,255) #(86,255,255) 
 
-# get dimensions
-imageHeight, imageWidth, imageChannels = Img.shape
+	#construct a mask
+	mask = cv2.inRange(hsv, greenLow, greenUp)
 
-# lire image en HSV
-hsv = cv2.cvtColor(Img, cv2.COLOR_BGR2HSV)
+	#Contours
+	contours= cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-# interval HSV of green
-#(120,100,50)
-greenLow = (40, 40,40) #(36,0,0) 
-greenUp = (70, 255,255) #(86,255,255) 
+	#choisir le contoure a le plus grand surface
+	contour = max(contours, key = cv2.contourArea)
 
-#construct a mask
-mask = cv2.inRange(hsv, greenLow, greenUp)
+	cv2.drawContours(Img, [contour], -1, 255, -1)
 
-#Contours
-contours= cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+	if disp:
+		cv2.imwrite('detectee.png',Img)
 
-#choisir le contoure a le plus grand surface
-contour = max(contours, key = cv2.contourArea)
+	M = cv2.moments(contour)
+	center  = (M["m10"]/M["m00"],M["m01"]/M["m00"])
+	area = cv2.contourArea(contour)
 
-cv2.drawContours(Img, [contour], -1, 255, -1)
+	x, y, w, h = cv2.boundingRect(contour)
+	return x, y, w, h
 
-cv2.imwrite('detectee.png',Img)
+if __name__ == "__main__":
 
-M = cv2.moments(contour)
-center  = (M["m10"]/M["m00"],M["m01"]/M["m00"])
-area = cv2.contourArea(contour)
-
-(x, y, w, h) = cv2.boundingRect(contour)
-print((x, y, w, h))
-
+	# read image from file
+	Img = cv2.imread("cylindre3.jpg")
+	x,y,w,h = get_bounding_box(Img, True)
+	print(x, y, w, h)
