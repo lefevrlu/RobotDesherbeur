dd19e7348bf6318cec456446b9b30117f83af1fc
Paul-Antoine <paul-antoine.grau@ensta-bretagne.org>
2019-01-30 17:47:50 +0100

    Position finie

--- gen_position/include/gen_position/gen_position.h
+++ gen_position/include/gen_position/gen_position.h
@@ -2,7 +2,16 @@
 #include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/Imu.h>
 #include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/Pose2D.h>
 #include <vector>
-#include <math.h> 
+#include <math.h>
 
-void callback(const sensor_msgs::LaserScan::ConstPtr& msgLIDAR, const sensor_msgs::Imu::ConstPtr& msgImu);
+static void toEulerAngle(const geometry_msgs::Quaternion & q);
+
+void callbackImu(const sensor_msgs::Imu::ConstPtr& msgImu);
+
+void callbackLidar(const sensor_msgs::LaserScan::ConstPtr& msgLIDAR);
+
+static void getPosition(double yaw, std::vector<float> ranges, double * pos_x, double * pos_y);
+
+static double modulo(double cap);
--- gen_position/src/gen_position.cpp
+++ gen_position/src/gen_position.cpp
@@ -1,9 +1,12 @@
 #include "gen_position.h"
 
 double angle_min, angle_max, angle_increment, range_min, range_max, cap, angularvelocityZ;
-std::vector<float> range;
+std::vector<float> ranges;
 bool firstTimeLidar = true;
 bool firstTimeImu = true;
+double roll, pitch, yaw;
+double pos_x = 0;
+double pos_y = 0;
 
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
@@ -15,66 +18,78 @@ void callbackLidar(const sensor_msgs::LaserScan::ConstPtr& msgLIDAR)
   angle_increment = msgLIDAR->angle_increment;
   range_min = msgLIDAR->range_min;
   range_max = msgLIDAR->range_max;
-  range = msgLIDAR->ranges;
+  ranges = msgLIDAR->ranges;
   firstTimeLidar = false;
 }
 
 void callbackImu(const sensor_msgs::Imu::ConstPtr& msgImu) {
-  cap = msgImu->orientation.w;
-  angularvelocityZ = msgImu->angular_velocity.z;
+  toEulerAngle(msgImu->orientation);
   firstTimeImu = false;
+}
+
+
+static void toEulerAngle(const geometry_msgs::Quaternion & q)
+{
+	// roll (x-axis rotation)
+	double sinr_cosp = +2.0 * (q.w * q.x + q.y * q.z);
+	double cosr_cosp = +1.0 - 2.0 * (q.x * q.x + q.y * q.y);
+	roll = atan2(sinr_cosp, cosr_cosp);
+
+	// pitch (y-axis rotation)
+	double sinp = +2.0 * (q.w * q.y - q.z * q.x);
+	if (fabs(sinp) >= 1)
+		pitch = copysign(M_PI / 2, sinp); // use 90 degrees if out of range
+	else
+		pitch = asin(sinp);
+
+	// yaw (z-axis rotation)
+	double siny_cosp = +2.0 * (q.w * q.z + q.x * q.y);
+	double cosy_cosp = +1.0 - 2.0 * (q.y * q.y + q.z * q.z);
+	yaw = atan2(siny_cosp, cosy_cosp);
+}
+
+static double modulo(double cap){
+  return fmod(fmod(cap,2*M_PI)+2*M_PI,2*M_PI);
+}
+
+static void getPosition(double yaw, std::vector<float> ranges, double * pos_x, double * pos_y){
+  double north = modulo(-yaw);
+  double east = modulo(M_PI/2 - yaw);
+  double south = modulo(M_PI - yaw);
+  double west = modulo(3*M_PI/2 - yaw);
+  int northInd = int(north / angle_increment);
+  int eastInd = int(east / angle_increment);
+  int southInd = int(south / angle_increment);
+  int westInd = int(west / angle_increment);
+  double high = ranges.at(northInd) + ranges.at(southInd);
+  double width = ranges.at(eastInd) + ranges.at(westInd);
+
+  *pos_x = ranges.at(southInd) - high/2;
+  *pos_y = ranges.at(westInd) - width/2;
 }
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
   ros::init(argc, argv, "gen_position");
-
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle n;
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
   ros::Subscriber subLidar = n.subscribe("/scan", 1000, callbackLidar);
-
   ros::Subscriber subImu = n.subscribe("/imu", 1000, callbackImu);
-
   ros::Rate r(5);
+  ros::Publisher chatter_pub = n.advertise<geometry_msgs::Pose2D>("/position", 1000);
 
+  geometry_msgs::Pose2D msg;
     // Main loop.
   while (n.ok()){
-    if (!firstTimeLidar) {
-      printf("I heard from LIDAR: [%f]\n", range.at(0));
+    if (!firstTimeImu && !firstTimeLidar){
+      getPosition(yaw,ranges,&pos_x, &pos_y);
+      msg.x = pos_x;
+      msg.y = pos_y;
+      msg.theta = yaw;
+
+      chatter_pub.publish(msg);
     }
-    if (!firstTimeImu) {
-      printf("I heard from IMU: [%f]\n", angularvelocityZ);
-    }
+    r.sleep();
     ros::spinOnce();
   }
 
