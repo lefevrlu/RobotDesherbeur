f7eda9ee740cc70cb54ec95d9af40400ba5e23cd
Chojnacki Alexandre <alexandre.04@hotmail.fr>
2019-01-30 09:48:31 +0100

    added my changes to main branch - wheel control script in dev

--- pilotage/robotControl.py
+++ pilotage/robotControl.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 def f(x,u):
+	"""
+	input:
+		x: [x,y,θ,v] vecteur d'état avec:
+			x, y: position
+			θ: orientation par rapport au Nord
+			v: la vitesse
+	output:
+	f renvoie dx/dt
+	"""
     x = x.flatten()
     u = u.flatten()
     return np.array([[x[3] * np.cos(x[2])],
